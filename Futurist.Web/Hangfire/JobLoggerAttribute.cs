01fe7e0ef0ff68e5f78cefb9095d79f45a44e137
Chaidir Ali Assegaf <ading.assegaf@gmail.com>
2025-03-27 12:16:28 +0700

    feat: Implement job logging with Serilog and Hangfire integration (#4)

---
+++ Futurist.Web/Hangfire/JobLoggerAttribute.cs
@@ -0,0 +1,60 @@
+using Futurist.Service.Dto;
+using Futurist.Service.Dto.Common;
+using Hangfire.Common;
+using Hangfire.Server;
+using Hangfire.States;
+using Serilog;
+using ILogger = Serilog.ILogger;
+
+namespace Futurist.Web.Hangfire;
+
+public class JobLoggerAttribute : JobFilterAttribute, IElectStateFilter, IServerFilter
+{
+    private readonly ILogger _logger = Log.ForContext<JobLoggerAttribute>();
+
+    public void OnStateElection(ElectStateContext context)
+    {
+        if (context.BackgroundJob.Job.Method.Name.Contains("Notify"))
+            return;
+
+        if (context.CandidateState is EnqueuedState)
+        {
+            _logger.Information("Job {JobId} with method {MethodName} has been {Status}", context.BackgroundJob.Id, context.BackgroundJob.Job.Method.Name, "Enqueued");
+        }
+    }
+
+    public void OnPerforming(PerformingContext context)
+    {
+        // No implementation needed
+    }
+
+    public void OnPerformed(PerformedContext context)
+    {
+        // Skip logging for notification jobs
+        if (context.BackgroundJob.Job.Method.Name.Contains("Notify"))
+            return;
+
+        var methodName = context.BackgroundJob.Job.Method.Name;
+        var roomId = context.BackgroundJob.Job.Args[0];
+
+        switch (context.Result)
+        {
+            case ServiceResponse<SpTaskDto> resultTaskSp when context.Exception != null:
+                _logger.Error(context.Exception, "JobId: {JobId}, {MethodName} with Room {RoomId} {Status}, Result: {Result}", context.BackgroundJob.Id, methodName, roomId, "Failed", string.Join(", ", resultTaskSp.Errors));
+                break;
+            // map the context.Result to a dictionary
+            case ServiceResponse<SpTaskDto> { Data.StatusId: false } resultTaskSp:
+                _logger.Error("JobId: {JobId}, {MethodName} with Room {RoomId} {Status}, Result: {Result}", context.BackgroundJob.Id, methodName, roomId, "Failed", resultTaskSp.Data?.StatusName);
+                break;
+            case ServiceResponse<SpTaskDto> resultTaskSp:
+                _logger.Information("JobId: {JobId}, {MethodName} with Room {RoomId} {Status}, Result: {Result}", context.BackgroundJob.Id, methodName, roomId, "Succeeded", resultTaskSp.Data?.StatusName);
+                break;
+            case ServiceResponse<string> resultString when context.Exception != null:
+                _logger.Error(context.Exception, "JobId: {JobId}, {MethodName} with Room {RoomId} {Status}, Result: {Result}", context.BackgroundJob.Id, methodName, roomId, "Failed", resultString.Errors);
+                break;
+            case ServiceResponse<string> resultString:
+                _logger.Information("JobId: {JobId}, {MethodName} with Room {RoomId} {Status}, Result: {Result}", context.BackgroundJob.Id, methodName, roomId, "Succeeded", resultString.Data);
+                break;
+        }
+    }
+}
