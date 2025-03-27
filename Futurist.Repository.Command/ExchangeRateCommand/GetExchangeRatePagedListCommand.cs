8096f39653723fad58b67fb9bfe0b30750c6e4a0
Chaidir Ali Assegaf <ading.assegaf@gmail.com>
2025-03-27 12:38:51 +0700

    feat(ExchangeRate): Add commands and repository interface for managing exchange rates

---
+++ Futurist.Repository.Command/ExchangeRateCommand/GetExchangeRatePagedListCommand.cs
@@ -0,0 +1,8 @@
+﻿using Futurist.Domain.Common;
+
+namespace Futurist.Repository.Command.ExchangeRateCommand;
+
+public class GetExchangeRatePagedListCommand : BaseCommand
+{
+    public PagedListRequest PagedListRequest { get; set; } = new();
+}
