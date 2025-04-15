7994a8a23b3ee09e793eb0f9637d2783b87a1a91
Chaidir Ali Assegaf <ading.assegaf@gmail.com>
2025-04-15 09:57:59 +0700

    feat(JobMonitoring): Implement Job Monitoring service, controller, and views; add data retrieval and error handling (#21)

---
+++ Futurist.Service.Interface/IJobMonitoringService.cs
@@ -0,0 +1,13 @@
+﻿using Futurist.Service.Dto;
+using Futurist.Service.Dto.Common;
+using Hangfire.Storage.Monitoring;
+
+namespace Futurist.Service.Interface;
+
+public interface IJobMonitoringService
+{
+    Task<ServiceResponse<JobMonitoringDto>> GetJobMonitoringAsync(int id);
+
+    Task<ServiceResponse<PagedListDto<JobMonitoringDto>>> GetJobMonitoringPagedListAsync(
+        PagedListRequestDto request);
+}
