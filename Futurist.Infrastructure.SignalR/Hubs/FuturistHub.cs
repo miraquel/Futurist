b809de1c3916190e2f3f4b330b00c5019cbde5ee
Chaidir Ali Assegaf <ading.assegaf@gmail.com>
2025-04-15 09:07:34 +0700

    feat(FuturistHub): Add FgCostService dependency and implement GetFgCostInProcessRoomIds method

--- Futurist.Infrastructure.SignalR/Hubs/FuturistHub.cs
+++ Futurist.Infrastructure.SignalR/Hubs/FuturistHub.cs
@@ -9,11 +9,13 @@ public class FuturistHub : Hub
 {
     private readonly IMupService _mupService;
     private readonly IBomStdService _bomStdService;
+    private readonly IFgCostService _fgCostService;
 
-    public FuturistHub(IMupService mupService, IBomStdService bomStdService)
+    public FuturistHub(IMupService mupService, IBomStdService bomStdService, IFgCostService fgCostService)
     {
         _mupService = mupService;
         _bomStdService = bomStdService;
+        _fgCostService = fgCostService;
     }
     
     public Task<IEnumerable<int>> GetMupInProcessRoomIds()
@@ -24,5 +26,10 @@ public class FuturistHub : Hub
     public Task<IEnumerable<int>> GetBomStdInProcessRoomIds()
     {
         return Task.FromResult(_bomStdService.GetBomStdInProcessRoomIds());
+    }
+    
+    public Task<IEnumerable<int>> GetFgCostInProcessRoomIds()
+    {
+        return Task.FromResult(_fgCostService.GetFgCostInProcessRoomIds());
     }
 }
