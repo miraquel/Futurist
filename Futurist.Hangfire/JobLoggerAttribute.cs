cfb348d2d837e348597833a817c546871396ad64
Chaidir Ali Assegaf <ading.assegaf@gmail.com>
2025-04-16 16:35:15 +0700

    feat(Hangfire): Initialize Hangfire project with logging and job management setup (#27)

---
+++ Futurist.Hangfire/JobLoggerAttribute.cs
@@ -0,0 +1,66 @@
+﻿using Futurist.Service.Dto;
+using Futurist.Service.Dto.Common;
+using Hangfire.Common;
+using Hangfire.Server;
+using Hangfire.States;
+using Serilog;
+using ILogger = Serilog.ILogger;
+
+namespace Futurist.Hangfire;
+
+public class JobLoggerAttribute : JobFilterAttribute, IElectStateFilter, IServerFilter
+{
+    private readonly ILogger _logger = Log.ForContext<JobLoggerAttribute>();
+
+    public void OnStateElection(ElectStateContext context)
+    {
+        if (context.BackgroundJob.Job.Method.Name.Contains("Notify"))
+            return;
+
+        if (context.CandidateState is EnqueuedState)
+        {
+            _logger.Information("JobId: {JobId}, {MethodName} with Room {RoomId} {Status}, Result: {Result}", context.BackgroundJob.Id, context.BackgroundJob.Job.Method.Name, context.BackgroundJob.Job.Args[0], "Enqueued", "Waiting for processing");
+        }
+    }
+
+    public void OnPerforming(PerformingContext context)
+    {
+        if (context.BackgroundJob.Job.Method.Name.Contains("Notify"))
+            return;
+        
+        var methodName = context.BackgroundJob.Job.Method.Name;
+        var roomId = context.BackgroundJob.Job.Args[0];
+        
+        _logger.Information("JobId: {JobId}, {MethodName} with Room {RoomId} {Status}, Result: {Result}", context.BackgroundJob.Id, methodName, roomId, "Started", "Processing");
+    }
+
+    public void OnPerformed(PerformedContext context)
+    {
+        // Skip logging for notification jobs
+        if (context.BackgroundJob.Job.Method.Name.Contains("Notify"))
+            return;
+
+        var methodName = context.BackgroundJob.Job.Method.Name;
+        var roomId = context.BackgroundJob.Job.Args[0];
+
+        switch (context.Result)
+        {
+            case ServiceResponse<SpTaskDto> resultTaskSp when context.Exception != null:
+                _logger.Error(context.Exception, "JobId: {JobId}, {MethodName} with Room {RoomId} {Status}, Result: {Result}", context.BackgroundJob.Id, methodName, roomId, "Failed", string.Join(", ", resultTaskSp.Errors));
+                break;
+            // map the context.Result to a dictionary
+            case ServiceResponse<SpTaskDto> { Data.StatusId: false } resultTaskSp:
+                _logger.Error("JobId: {JobId}, {MethodName} with Room {RoomId} {Status}, Result: {Result}", context.BackgroundJob.Id, methodName, roomId, "Failed", resultTaskSp.Data?.StatusName);
+                break;
+            case ServiceResponse<SpTaskDto> resultTaskSp:
+                _logger.Information("JobId: {JobId}, {MethodName} with Room {RoomId} {Status}, Result: {Result}", context.BackgroundJob.Id, methodName, roomId, "Succeeded", resultTaskSp.Data?.StatusName);
+                break;
+            case ServiceResponse<string> resultString when context.Exception != null:
+                _logger.Error(context.Exception, "JobId: {JobId}, {MethodName} with Room {RoomId} {Status}, Result: {Result}", context.BackgroundJob.Id, methodName, roomId, "Failed", resultString.Errors);
+                break;
+            case ServiceResponse<string> resultString:
+                _logger.Information("JobId: {JobId}, {MethodName} with Room {RoomId} {Status}, Result: {Result}", context.BackgroundJob.Id, methodName, roomId, "Succeeded", resultString.Data);
+                break;
+        }
+    }
+}
---
+++ Futurist.Hangfire/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Data;
+using Futurist.Hangfire;
+using Futurist.Repository.SqlServer;
+using Futurist.Repository.UnitOfWork;
+using Futurist.Service;
+using Hangfire;
+using Serilog;
+using Serilog.Filters;
+using Serilog.Sinks.MSSqlServer;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddRepositorySqlServer();
+builder.Services.AddUnitOfWork();
+builder.Services.AddServices();
+
+// Add Serilog
+var serilogConfiguration = new LoggerConfiguration();
+
+serilogConfiguration.ReadFrom.Configuration(builder.Configuration);
+serilogConfiguration.Enrich.FromLogContext();
+if (builder.Environment.IsDevelopment())
+{
+    serilogConfiguration.WriteTo.Console();
+}
+
+serilogConfiguration.WriteTo.Logger(lc => lc
+    .Filter.ByIncludingOnly(Matching.FromSource("Futurist.Hangfire.JobLoggerAttribute"))
+    .WriteTo.MSSqlServer(builder.Configuration.GetConnectionString("SerilogConnection"), 
+        sinkOptions: new MSSqlServerSinkOptions
+        {
+            TableName = "Logs",
+            AutoCreateSqlTable = true,
+        },
+        columnOptions: new ColumnOptions
+        {
+            AdditionalColumns =
+            [
+                new SqlColumn { DataType = SqlDbType.VarChar, ColumnName = "SourceContext" }
+            ]
+        }
+    )
+);
+
+serilogConfiguration.WriteTo.Logger(lc => lc
+    .Filter.ByIncludingOnly(Matching.FromSource("Futurist.Hangfire.JobLoggerAttribute"))
+    .WriteTo.MSSqlServer(builder.Configuration.GetConnectionString("SerilogConnection"), 
+        sinkOptions: new MSSqlServerSinkOptions
+        {
+            TableName = "JobLogs",
+            AutoCreateSqlTable = true,
+        },
+        columnOptions: new ColumnOptions
+        {
+            AdditionalColumns =
+            [
+                new SqlColumn { DataType = SqlDbType.VarChar, ColumnName = "SourceContext" },
+                new SqlColumn { DataType = SqlDbType.VarChar, ColumnName = "Status" },
+                new SqlColumn { DataType = SqlDbType.VarChar, ColumnName = "JobId" }
+            ]
+        }
+    )
+);
+
+var serilog = serilogConfiguration.CreateLogger();
+
+Log.Logger = serilog;
+
+builder.Services.AddLogging(loggingBuilder => loggingBuilder.AddSerilog(serilog));
+
+// Add Hangfire services.
+builder.Services.AddHangfire(configuration => configuration
+    .UseFilter(new JobLoggerAttribute())
+    .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+    .UseSimpleAssemblyNameTypeSerializer()
+    .UseRecommendedSerializerSettings()
+    .UseSqlServerStorage(builder.Configuration.GetConnectionString("HangfireConnection")));
+
+// Add the processing server as IHostedService
+builder.Services.AddHangfireServer();
+
+builder.Services.AddHttpClient<KeycloakTokenService>();
+
+var host = builder.Build();
+host.Run();
