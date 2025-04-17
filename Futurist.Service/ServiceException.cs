320216f61dd27de8e21260e213b5f4f62e6d754c
Chaidir Ali Assegaf <ading.assegaf@gmail.com>
2025-04-17 08:11:14 +0700

    feat(Service): Add new services for job monitoring, exchange rate, item adjustment, and update ServiceException formatting

--- Futurist.Service/ServiceException.cs
+++ Futurist.Service/ServiceException.cs
@@ -1,50 +1,49 @@
 ﻿using System.Net;
 
-namespace Futurist.Service
+namespace Futurist.Service;
+
+/// <summary>
+/// Represents errors that occur within the service layer.
+/// </summary>
+public class ServiceException : Exception
 {
     /// <summary>
-    /// Represents errors that occur within the service layer.
+    /// Gets the HTTP status code that is associated with the exception.
     /// </summary>
-    public class ServiceException : Exception
+    public HttpStatusCode StatusCode { get; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ServiceException"/> class.
+    /// </summary>
+    public ServiceException() { }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ServiceException"/> class with a specified error message.
+    /// </summary>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
+    public ServiceException(string message)
+        : base(message) { }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ServiceException"/> class with a specified error message and HTTP status code.
+    /// </summary>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
+    /// <param name="statusCode">The HTTP status code to associate with the exception.</param>
+    public ServiceException(string message, HttpStatusCode statusCode)
+        : base(message)
     {
-        /// <summary>
-        /// Gets the HTTP status code that is associated with the exception.
-        /// </summary>
-        public HttpStatusCode StatusCode { get; }
+        StatusCode = statusCode;
+    }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ServiceException"/> class.
-        /// </summary>
-        public ServiceException() { }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ServiceException"/> class with a specified error message.
-        /// </summary>
-        /// <param name="message">The error message that explains the reason for the exception.</param>
-        public ServiceException(string message)
-            : base(message) { }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ServiceException"/> class with a specified error message and HTTP status code.
-        /// </summary>
-        /// <param name="message">The error message that explains the reason for the exception.</param>
-        /// <param name="statusCode">The HTTP status code to associate with the exception.</param>
-        public ServiceException(string message, HttpStatusCode statusCode)
-            : base(message)
-        {
-            StatusCode = statusCode;
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ServiceException"/> class with a specified error message, a reference to the inner exception that is the cause of this exception, and an HTTP status code.
-        /// </summary>
-        /// <param name="message">The error message that explains the reason for the exception.</param>
-        /// <param name="statusCode">The HTTP status code to associate with the exception.</param>
-        /// <param name="innerException">The exception that is the cause of the current exception.</param>
-        public ServiceException(string message, HttpStatusCode statusCode, Exception innerException)
-            : base(message, innerException)
-        {
-            StatusCode = statusCode;
-        }
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ServiceException"/> class with a specified error message, a reference to the inner exception that is the cause of this exception, and an HTTP status code.
+    /// </summary>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
+    /// <param name="statusCode">The HTTP status code to associate with the exception.</param>
+    /// <param name="innerException">The exception that is the cause of the current exception.</param>
+    public ServiceException(string message, HttpStatusCode statusCode, Exception innerException)
+        : base(message, innerException)
+    {
+        StatusCode = statusCode;
     }
 }
